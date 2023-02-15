a83cd9f80bfc2b53d6c8fa2f2948d2c2b4134a7a
MM <marekm4004@gmail.com>
2023-02-15 18:36:24 +0100

    Release 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pydiscordwebhook",
-    version='0.1.4',
+    version='1.0.0',
     author="MM4004",
     description='Library for python to send messages on discord',
     packages=find_packages(),
